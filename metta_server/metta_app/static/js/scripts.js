24a2d65269fc9814468fa49e92ea02551739cfb0
jhyeep <45350427+jhyeep@users.noreply.github.com>
2020-07-11 18:20:18 +0800

    calculator fields test

--- metta_server/metta_app/static/js/scripts.js
+++ metta_server/metta_app/static/js/scripts.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
   $("#growth_period_input").click(function () {
     alert("hello");
   });
+
+  $("#calculate").click(function () {
+    var target_conc = parseFloat($("#calc_conc_input").val())
+    var water_vol = parseFloat($("#calc_water_input").val())
+    var output = target_conc + water_vol;
+    // alert(target_conc + water_vol);
+    $("#nutrient_vol").text(output);
+  });
 });
-
-
